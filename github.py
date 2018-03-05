79f1185a79a3fe2cc0c0c9e410b82d15a13b29b9
Brian Lee <blee025432@gmail.com>
2018-03-04 23:30:05 -0500

    i can test for recent commits!

--- github.py
+++ github.py
@@ -1,22 +1,29 @@
 import requests
+import getpass
 import json
+import datetime
+
 url = 'https://api.github.com/user/repos'
 url_4 = 'https://api.github.com/graphql'
 
-def pull_v3(username,password):#lists full repo, NOT pinned repos
+def pull_v3(username,password):#lists full repo, NOT pinned repos, also needs to handle bad passwords
+	today = datetime.datetime.today()
+	margin = datetime.timedelta(days=30)
 	r = requests.get(url, auth=(username,password))
 	#print(r.url)
 	#print(json.dumps(r.json(), indent=4, sort_keys=True))
 	json_stuff = r.json()
 	for i in json_stuff:
-		print(i['name'])
-		print(i['language'])
-		print(i['updated_at'])
+		datetime_object = datetime.datetime.strptime(i['pushed_at'],"%Y-%m-%dT%H:%M:%SZ")
+		if(today-datetime_object<margin):
+			print(i['name'])
+			print(i['language'])
+			print(datetime_object)
 def pull_v4(): #need to resolve oauth problems
 	r = requets.get('https://github.com/login/oauth/authorize')
 	print(json.dumps(r.json(), indent=4, sort_keys=True))
 def user_enter():
 	username = input("Enter username: ")
-	password = input("Enter password: ")
+	password = getpass.getpass("Enter password: ")
 	pull_v3(username,password)
 user_enter()
