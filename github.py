eab74f75cbb6673f09ed9946b351e040e7ff28df
Brian Lee <blee025432@gmail.com>
2018-03-04 23:10:32 -0500

    added user enter

--- github.py
+++ github.py
@@ -1,8 +1,9 @@
 import requests
 import json
 url = 'https://api.github.com/user/repos'
+url_4 = 'https://api.github.com/graphql'
 
-def pull(username,password):
+def pull_v3(username,password):#lists full repo, NOT pinned repos
 	r = requests.get(url, auth=(username,password))
 	#print(r.url)
 	#print(json.dumps(r.json(), indent=4, sort_keys=True))
@@ -10,3 +11,12 @@ def pull(username,password):
 	for i in json_stuff:
 		print(i['name'])
 		print(i['language'])
+		print(i['updated_at'])
+def pull_v4(): #need to resolve oauth problems
+	r = requets.get('https://github.com/login/oauth/authorize')
+	print(json.dumps(r.json(), indent=4, sort_keys=True))
+def user_enter():
+	username = input("Enter username: ")
+	password = input("Enter password: ")
+	pull_v3(username,password)
+user_enter()
