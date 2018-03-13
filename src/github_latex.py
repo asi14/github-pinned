6bcc59329d2b8c3d9cd83c5dc05ce96b7e11ff0e
Brian Lee <blee025432@gmail.com>
2018-03-13 11:54:03 -0400

    need class

--- src/github_latex.py
+++ src/github_latex.py
@@ -1,19 +1,21 @@
 '''
 TODO: make into class
 A sample implementation of PyLaTeX with Github-Pinned to generate resumes
-
+take note with how the method github_generate(doc) ieterates through the outputed dictionary to extract the repo
+details as necessary
+for PyLaTeX-specific questions, please visit its documentation.
 '''
 import github
-from pylatex import Document, Section, Subsection, Command,Itemize
+from pylatex import Document, Section, Subsection, Command,Itemize,Package
 from pylatex.utils import italic, NoEscape
 
 #follow example and create latex document
-def make_repos():
-	return github.pull_margin(30)
 
-def preamble_modify(doc): #add packages this way doc.packages.append(Package('tikz'))
-	print('AAAA')	
-def github_generate(doc):
+def make_repos(): #generate github dictionary here
+	return github.pull_margin(30)
+def preamble_modify(doc): #add LaTeX packages here. Ex: doc.packages.append(Package('tikz'))
+	doc.packages.append(Package('myresume'))#sample package
+def github_generate(doc): #parses the file and generates the according LaTeX
 	output=make_repos()
 	with doc.create(Section('Github Projects')):
 		for key,values in output.items():
